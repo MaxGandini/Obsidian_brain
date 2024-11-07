f550b1c1709d92907fed497cf131af292f3c4f1d
Xilian <maximiliano.gandini.27@gmail.com>
2024-11-07 14:54:23 -0300

    vault backup: 2024-11-07 14:54:23
    Affected files:
    .obsidian/workspace.json
    AI/Machine_Learning/Supervised/linear_regression1.py

--- AI/Machine_Learning/Supervised/linear_regression1.py
+++ AI/Machine_Learning/Supervised/linear_regression1.py
@@ -8,4 +8,53 @@ import pandas as pd
 
 data = pd.read_csv('/home/Xilian/.cache/kagglehub/datasets/yasserh/housing-prices-dataset/versions/1/Housing.csv')
 
+import pandas as pd
+import seaborn.objects as so
+import sklearn
+import matplotlib.pyplot as plt
 
+sklearn.set_config(transform_output="pandas")
+target = "price"  
+
+X = data.drop(target,axis=1)
+y = data[target]
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import MinMaxScaler
+
+transformer = ColumnTransformer(
+    [
+        (
+            "category", 
+            OrdinalEncoder(),
+            ["mainroad","guestroom","basement","hotwaterheating","airconditioning","prefarea","furnishingstatus"],  
+        ),
+        (
+            "number",
+            MinMaxScaler(),
+            ["bedrooms","stories","bathrooms","area","parking"],
+        )
+    ],
+    remainder="passthrough",
+    #verbose_feature_names_out=True,
+)
+
+
+data_= pd.DataFrame(transformer.fit_transform(X), columns=transformer.get_feature_names_out())
+
+from sklearn.linear_model import LinearRegression
+
+X_train, y_train, X_test, y_test = test_train_split()
+model = LinearRegression()
+model.fit(data_, y)
+df["predicted"] = model.predict(data_)
+
+(
+    so.Plot(df, x="Study Hours")
+    .add(so.Line(), y="predicted", label="predicted")
+    .add(so.Dot(), y="Grade", label="Grade")
+    .label(y="Grade")
+    .show()
+)
+display(df)
