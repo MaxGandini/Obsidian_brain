6fe3d0fbbe0d28d4e407ae70bb6c40f342e830bb
MaxGandini <maximiliano.gandini.27@gmail.com>
2024-12-15 21:33:10 -0300

    vault backup: 2024-12-15 21:33:09
    Affected files:
    .obsidian/workspace.json
    kaggle_comp/Jane_street_time_series.py

--- kaggle_comp/Jane_street_time_series.py
+++ kaggle_comp/Jane_street_time_series.py
@@ -1,50 +1,16 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import psycopg2
 import os
+from pathlib import Path
+import pandas as pd 
 
-dbname = os.getenv("DB_NAME")
-dbuser = os.getenv("DB_USER")
-dbpassword = os.getenv("DB_PASSWORD")
-dbhost = os.getenv("DB_HOST")
-dbport = os.getenv("DB_PORT")
+grandparent_dir= Path(__file__).parents[2]
 
-try:
-    conn = psycopg2.connect(
-        dbname=dbname,
-        user=dbuser,
-        password=dbpassword,
-        host=dbhost,
-        port=dbport
-    )
-    
-    cursor = conn.cursor()
-    
-    # Write your query
-    query = "SELECT * FROM "
-    
-    # Execute the query
-    cursor.execute(query)
-    
-    # Fetch all rows
-    rows = cursor.fetchall()
-    
-    # Fetch column names
-    colnames = [desc[0] for desc in cursor.description]
-    
-    df = pd.DataFrame(rows, columns=colnames)
-    display(df)
-    display(df.columns)
-    display(df.info())
-    display(df.describe(include='all'))
+data_path = grandparent_dir / "datasets" / "part-0.parquet"
+data = pd.read_parquet(data_path)
 
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    if 'cursor' in locals():
-        cursor.close()
-    if 'conn' in locals():
-        conn.close()
-    print("Connection closed")
+import sklearn
+import matplotlib.pyplot as plt
 
+plt.plot(data['time_id'],data['feature_00'])
+plt.show()
