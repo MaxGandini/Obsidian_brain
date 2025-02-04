ab7609c082a162a4652b854f61fde89c61dc4fbe
MaxGandini <maximiliano.gandini.27@gmail.com>
2025-02-04 00:10:27 -0300

    vault backup: 2025-02-04 00:10:26
    Affected files:
    .obsidian/core-plugins.json
    .obsidian/workspace.json
    AI/Generative_AI/LLM/LLM_course/Week1/day5.py

--- AI/Generative_AI/LLM/LLM_course/Week1/day5.py
+++ AI/Generative_AI/LLM/LLM_course/Week1/day5.py
@@ -1,39 +1,44 @@
-import os
-import requests
 import json
+import os
 from typing import List
-from dotenv import load_dotenv
+
+import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from IPython.display import Markdown, display, update_display
 from openai import OpenAI
 
 load_dotenv()
 
-OPENAI_API_KEY= os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-model = 'gpt-4o-mini'
+model = "gpt-4o-mini"
 openai = OpenAI()
 
+
 class Website:
-    '''
-    We can represent a website with this class 
-    '''
+    """
+    We can represent a website with this class
+    """
+
     def __init__(self, url):
         self.url = url
         response = requests.get(url)
         self.body = response.content
-        soup = BeautifulSoup(self.body, 'html.parser')
-        self.title = soup.title.string if soup.title else 'no_title_provided'
+        soup = BeautifulSoup(self.body, "html.parser")
+        self.title = soup.title.string if soup.title else "no_title_provided"
         if soup.body:
-            for irrelevant in soup.body(['script','style','img','input']):
+            for irrelevant in soup.body(["script", "style", "img", "input"]):
                 irrelevant.decompose()
             self.text = soup.body.get_text(separator="\n", strip=True)
         else:
             self.text = ""
-        links = [link.get('href') for link in soup.find_all('a')]
+        links = [link.get("href") for link in soup.find_all("a")]
         self.links = [link for link in links if link]
+
     def get_contents(self):
-        return f'Webpage title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n'
+        return f"Webpage title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n"
+
 
 git_max = Website("https://github.com/MaxGandini")
 print(git_max.links)
@@ -55,8 +60,10 @@ link_system_prompt += """
 
 def get_links_user_prompt(website):
     user_prompt = f"Here is the list of links on the website of {website.url} - "
-    user_prompt += "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
+    user_prompt += (
+        "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
 Do not include Terms of Service, Privacy, email links.\n"
+    )
     user_prompt += "Links (some might be relative links):\n"
     user_prompt += "\n".join(website.links)
     return user_prompt
@@ -64,16 +71,26 @@ Do not include Terms of Service, Privacy, email links.\n"
 def get_links(url):
     website = Website(url)
     response = openai.chat.completions.create(
-            model=model,
-            messages=[
-                {"role": "system", "content": link_system_prompt},
-                {"role": "user", "content": get_links_user_prompt(website)}
-                ],
-            response_format={"type": "json_object"}
+        model=model,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(website)},
+        ],
+        response_format={"type": "json_object"},
     )
     result = response.choices[0].message.content
     return json.loads(result)
 
+
 print(get_links("https://anthropic.com"))
 
+def get_all_details(url):
+    result = 'Landing page:\n'
+    result += Website(url).get_contents()
+    links = get_links(url)
+    print("found links:", links)
+    for link in links['links']:
+        result += f"\n\n{link['type']}\n"
+        result +=Website(link["url"]).get_contents()
 
+    return result
